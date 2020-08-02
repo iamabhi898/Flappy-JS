688a3ce6f846d34a65af43a85615e1d4fae73e65
Abhishek Tripathi <59258140+iamabhi898@users.noreply.github.com>
2020-08-02 16:55:44 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -33,7 +33,7 @@ function Ball() {
 
     this.hop = function (event) {
         if (event.key === " " || event.type === "mousedown") {
-            this.yVel += -15;
+            this.yVel += -20;
         }
     }
 
